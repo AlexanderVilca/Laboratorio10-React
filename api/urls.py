1afac9d9d3c3c2d2c49c175e27f89f4f4154e758
AlexanderVilca <130238718+AlexanderVilca@users.noreply.github.com>
2023-11-21 20:32:25 -0500

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -12,5 +12,6 @@ urlpatterns = [
     path('categoria',views.CategoriaView.as_view()),
     path('producto',views.ProductoView.as_view()),
     path('categoria/<int:categoria_id>',views.CategoriaDetailView.as_view()),
+    path('producto/<int:producto_id>',views.ProductoDetailView.as_view()),
     path('admin/',include(router.urls))
 ]
