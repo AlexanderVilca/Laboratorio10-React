2b651e93a0061ea5656b6e1ef85b357e11b9218c
AlexanderVilca <130238718+AlexanderVilca@users.noreply.github.com>
2023-11-22 22:31:08 -0500

    Update views.py

--- api/views.py
+++ api/views.py
@@ -2,6 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import generics
 from rest_framework import viewsets
+from rest_framework import status
 
 from tienda.models import Categoria,Producto
 from .serializers import (
